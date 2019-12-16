7ce5b8683084f93aa488c23953190aabf7964d3f
Joon Lim <joonlim@google.com>
2019-12-16 11:10:49 -0500

    Test.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask('test-app')
 
 @app.route('/')
 def hello():
-  return 'Hello, World!!!!!\n'
+  return 'Hello, Folks!!!!!\n'
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
